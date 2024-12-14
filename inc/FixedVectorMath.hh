bc97628ed97be588c212b3db309f95b298c47c19
etredal <etredal@gmail.com>
2024-12-13 22:11:50 -0800

    FV move

--- inc/FixedVectorMath.hh
+++ inc/FixedVectorMath.hh
@@ -1,11 +1,12 @@
 #ifndef FIXED_VECTOR_MATH_H
 #define FIXED_VECTOR_MATH_H
 
-#include "../utils/FixedVector.hh"
+#include "FixedVector.hh"
 #include <cmath> // For sqrt and pow
 
-namespace {
 
+
+namespace {
     // Transpose operation for a matrix (2D FixedVector)
     template <typename T>
     FixedVector<FixedVector<T>> transpose(const FixedVector<FixedVector<T>>& matrix);
